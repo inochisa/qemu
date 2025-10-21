6ba3d2d86c364a5661d8e3b0df9f7357eaaa6220
Inochi Amaoto <inochiama@gmail.com>
2025-10-21 10:54:07 +0800

    exp: target: riscv: record shadow fence.
    Signed-off-by: Inochi Amaoto <inochiama@gmail.com>

--- target/riscv/op_helper.c
+++ target/riscv/op_helper.c
@@ -648,6 +648,10 @@ static void do_spte_flush(CPURISCVState *env, target_ulong flush_asid,
 
     qemu_log_mask(CPU_LOG_SMMU | CPU_LOG_SMA, "SMMU: fence flush on " TARGET_FMT_lx "(" TARGET_FMT_lu ")\n", flush_vaddr, flush_asid);
 
+	if (flush_vaddr != 0) {
+        env->shadow_fence++;
+    }
+
     riscv_cpu_flush_all_valid_map(env, NULL);
     return;
 
@@ -655,8 +659,6 @@ static void do_spte_flush(CPURISCVState *env, target_ulong flush_asid,
 	// 	riscv_cpu_flush_all_valid_map(env, NULL);
 	// 	return;
 	// }
-
-    // env->shadow_fence++;
 
 	// riscv_get_shadow_physical_address(env, NULL, &ret_prot,
     //                                   (hwaddr)flush_vaddr, NULL, true, false);
